93e85bc8a35127d1c6233acc4a70190995fb09d8
LP_PCPC_Master <634210653@qq.com>
2019-07-28 10:54:22 +0800

    update test

--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -2,5 +2,31 @@
  * @Author: LP
  * @Date: 2019/7/28
  */
+
 public class BibliotecaTest {
+
+    public void  shouldHaveWelcomeMessage(){
+
+    }
+
+    public void  shouldRecordBooks(){
+
+    }
+
+    public void shouldHaveMainMenuWhenInivaOptionSayInivaMessage(){
+
+    }
+
+    public void shouldListBooksHaveAuthorAndYear(){
+
+    }
+
+    public void shouldCheckoutBooksHaveSuccessAndFailureMessage(){
+
+    }
+
+    public void shouldReturnBooksHaveSuccessAndFailureMesseage(){
+
+    }
+
 }
