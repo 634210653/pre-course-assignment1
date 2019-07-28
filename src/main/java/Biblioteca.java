a0eb0ee93897c37900a61311b43af253ebad1760
LP_PCPC_Master <634210653@qq.com>
2019-07-28 13:40:55 +0800

    version1

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,7 +1,27 @@
+import java.util.HashMap;
+
 /**
  * @Author: LP
  * @Date: 2019/7/28
  */
+
+
 public class Biblioteca {
 
+    private HashMap<Book,Boolean> books;
+
+    public Biblioteca(HashMap<Book,Boolean>books){
+        this.books = books;
+    }
+
+    public String welcome(){
+
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public Boolean isBookAvailable(Book book){
+        if(book == null||!books.containsKey(book))
+            return false;
+        return books.get(book);
+    }
 }
--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -1,15 +1,42 @@
+import java.util.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * @Author: LP
  * @Date: 2019/7/28
  */
 
+
+
 public class BibliotecaTest {
 
+    @Test
     public void  shouldHaveWelcomeMessage(){
 
+        HashMap<Book,Boolean> books = new HashMap<>();
+        books.put(new Book("book1","author1",1992),true);
+        Biblioteca lib = new Biblioteca(books);
+        String expected = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        String actual = lib.welcome();
+        Assert.assertEquals(expected,actual);
     }
 
+    @Test
     public void  shouldRecordBooks(){
+
+        HashMap<Book,Boolean> books = new HashMap<>();
+        Book book1 = new Book("book1","author1",1992);
+        Book book2 = new Book("book2","author2",1993);
+        books.put(book1,true);
+        books.put(book2,false);
+        Biblioteca lib = new Biblioteca(books);
+        Assert.assertTrue(lib.isBookAvailable(book1));
+        Assert.assertFalse(lib.isBookAvailable(book2));
+        Assert.assertFalse(lib.isBookAvailable(new Book("book1","author1",1993)));
 
     }
 
